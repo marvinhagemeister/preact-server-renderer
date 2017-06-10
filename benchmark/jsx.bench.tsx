530fe5a9080161448452a3e9bff201a42005b8ab
Marvin Hagemeister <marvin@marvinhagemeister.de>
2017-06-10 15:12:51 +0200

    Add more benchmarking

--- benchmark/jsx.bench.tsx
+++ benchmark/jsx.bench.tsx
@@ -3,8 +3,9 @@ import { h } from "preact";
 import renderSync from "../src/renderSync";
 import JsxRenderer from "../src/JsxRenderer";
 import { view } from "./components";
+import * as Bench2 from "benchmark";
 
-/* tslint:disable:no-var-requires */
+/* tslint:disable:no-var-requires no-console */
 const renderToString = require("preact-render-to-string/jsx");
 
 const syncRenderer = new JsxRenderer(() => {
@@ -13,7 +14,20 @@ const syncRenderer = new JsxRenderer(() => {
 
 export default async function bench() {
   await new Benchmark()
+    .add("preact-stream-renderer", () => renderSync(view, syncRenderer))
+    .add("preact-render-to-string", () => renderToString(view))
+    .run();
+}
+
+export function bench2() {
+  new Bench2.Suite()
     .add("preact-render-to-string", () => renderToString(view))
     .add("preact-stream-renderer", () => renderSync(view, syncRenderer))
-    .run();
+    .on("cycle", (event: any) => {
+      console.log(String(event.target));
+    })
+    .on("complete", function() {
+      console.log("Fastest is " + this.filter("fastest").map("name"));
+    })
+    .run({ async: true });
 }
