3f2452dc7b6f9153a27637c02aeec93856269dcf
Marvin Hagemeister <marvin@marvinhagemeister.de>
2017-11-24 18:08:33 +0100

    Cleanup dependencies

--- benchmark/helpers.ts
+++ benchmark/helpers.ts
@@ -1,24 +1,8 @@
-import * as chalk from "chalk";
-import * as Benchmark from "benchmark";
-
-/* tslint:disable no-console */
-export const logWinner = (suite: any) =>
-  console.log(
-    "Fastest is " + chalk.green(suite.filter("fastest").map("name")) + "\n",
-  );
-
-export const logCycle = (event: any) => console.log(String(event.target));
+import Benchmark from "benchmarkjs-pretty";
 
 export function createBenchmark(name: string, a: () => any, b: () => any) {
-  return new Promise(resolve => {
-    new Benchmark.Suite("Compact")
-      .add("preact-render-to-string", a)
-      .add("preact-stream-renderer", b)
-      .on("cycle", logCycle)
-      .on("complete", function(this: any) {
-        logWinner(this);
-        resolve();
-      })
-      .run({ async: true });
-  });
+  new Benchmark(name)
+    .add("preact-render-to-string", a)
+    .add("preact-server-renderer", b)
+    .run();
 }
